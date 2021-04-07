cab1b09531fdc4698dbcf18c570608b1c4aaa2f9
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-07 07:33:20 -0300

    Documentado.

--- Archivos_CRUD/AbstractArchivo.php
+++ Archivos_CRUD/AbstractArchivo.php
@@ -1,11 +1,30 @@
 <?php
 
+    /**
+     * Clase que permitirá manejar la creación de archivos para Alta, Baja y Modificación de objetos en el mismo.
+     * @abstract
+     * @author Varela Vargas Leandro Gastón
+     */
     abstract class AbstractArchivo {
 
+        /**
+         * Atributo que contendrá el PATH hacia el archivo.
+         * @var String
+         */
         private $str_pathToFile;
+
+        /**
+         * Atributo que contendrá el TIPO de archivo.
+         * @var String
+         */
         private $str_fileType;
 
-        public function __construct ( $str_pathToFile, $str_fileType ) {
+        /**
+         * Función constructura de la clase.
+         * @param String $str_pathToFile PATH del archivo.
+         * @param String $str_fileType TIPO de archivo.
+         */
+        protected function __construct ( $str_pathToFile, $str_fileType ) {
             
             $isAnyNull = !(isset($str_pathToFile) && isset($str_fileType));
             
@@ -24,15 +43,21 @@
 
         }
 
+        /**
+         * Getter del atributo str_pathToFile.
+         * @return String atributo str_pathToFile.
+         */
         public function getPathToFile() {
             return $this->str_pathToFile;
         }
 
+        /**
+         * Getter del atributo str_fileType.
+         * @return String atributo str_fileType.
+         */
         public function getFileType() {
             return $this->str_fileType;
         }
-
-        
 
     }
 
