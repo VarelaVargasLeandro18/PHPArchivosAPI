efb5cb49024f36f223c9836ae40191ba12a1b5da
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-07 01:42:41 -0300

    Coomienzo de creación de la clase abstracta 'AbstractArchivo'

--- Archivos_CRUD/AbstractArchivo.php
+++ Archivos_CRUD/AbstractArchivo.php
@@ -1,5 +1,17 @@
 <?php
 
-    
+    abstract class AbstractArchivo {
+
+        private $file;
+        private $str_pathToFile;
+        private $str_fileType;
+
+        public function __construct ( $file, $str_pathToFile, $str_fileType ) {
+            
+            
+
+        }
+
+    }
 
 ?>
