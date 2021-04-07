1f2fa3ddb54b5167fbf2ff8eab50b9a7aa95b7c0
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-07 07:08:16 -0300

    Eliminado atributo innecesario .

--- Archivos_CRUD/AbstractArchivo.php
+++ Archivos_CRUD/AbstractArchivo.php
@@ -2,31 +2,24 @@
 
     abstract class AbstractArchivo {
 
-        private $file;
         private $str_pathToFile;
         private $str_fileType;
 
-        public function __construct ( $file, $str_pathToFile, $str_fileType ) {
+        public function __construct ( $str_pathToFile, $str_fileType ) {
             
-            $isAnyNull = !(isset($file) && isset($str_pathToFile) && isset($str_fileType));
+            $isAnyNull = !(isset($str_pathToFile) && isset($str_fileType));
             
             if ( $isAnyNull )
                 throw new Exception("Ningún atributo puede ser NULL.");
-
-            $this->file = $file;
             
             $this->str_pathToFile = $str_pathToFile;
-            
-            if ( !str_ends_with($str_pathToFile, "/" . $file["name"]) ) { // Si el PATH NO termina con el nombre del archivo...
 
-                if ( !str_ends_with($str_pathToFile, "/") ) { // Si el PATH no termina con "/"...
-                    $this->str_pathToFile .= "/"; // Le agregamos "/"
-                }
-
-                $this->str_pathToFile .= $file["name"]; // Le agregamos el nombre del archivo.
-
+            if ( !( str_ends_with($str_pathToFile, $str_fileType) ) ) {
+                if ( !( str_ends_with($str_pathToFile, $str_fileType) ) ) 
+                    $this->str_pathToFile .= ".";
+                $this->str_pathToFile .= $str_fileType;
             }
-            
+
             $this->str_fileType = $str_fileType;
 
         }
