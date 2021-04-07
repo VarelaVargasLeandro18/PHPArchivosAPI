5d8ff431902dfad0bbd97f29a86d0d9c76555269
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-07 07:54:50 -0300

    Funciones para cerrar y agregar líneas creadas.

--- Archivos_CRUD/AbstractArchivo.class.php
+++ Archivos_CRUD/AbstractArchivo.class.php
@@ -20,6 +20,12 @@
         private $str_fileType;
 
         /**
+         * Atributo que contendrá el MODO en que se ha abierto el archivo.
+         * @var String;
+         */
+        private $str_mode;
+
+        /**
          * Función constructura de la clase.
          * @param String $str_pathToFile PATH del archivo.
          * @param String $str_fileType TIPO de archivo.
@@ -48,6 +54,7 @@
         /**
          * Getter del atributo str_pathToFile.
          * @return String atributo str_pathToFile.
+         * @author Varela Vargas Leandro.
          */
         public final function getPathToFile() {
             return $this->str_pathToFile;
@@ -56,6 +63,7 @@
         /**
          * Getter del atributo str_fileType.
          * @return String atributo str_fileType.
+         * @author Varela Vargas Leandro.
          */
         public final function getFileType() {
             return $this->str_fileType;
@@ -65,20 +73,34 @@
 
         /**
          * Función que permite la creación u apertura del archivo (de ser posible).
-         * @param String $modo modo en el que el archivo se abrirá.
-         * @return FILE|bool puntero al archivo. En caso de error devuelve false.
+         * @param String $str_mode modo en el que el archivo se abrirá.
+         * @return FILE|bool puntero al archivo. En caso de error devuelve FALSE.
+         * @author Varela Vargas Leandro.
          */
-        protected final function crear_abrirArchivo( $modo ) {
-            return fopen($this->str_pathToFile, $modo, false);
+        protected final function create_openFile( $str_mode ) {
+            $this->str_mode = $str_mode;
+            return fopen($this->str_pathToFile, $str_mode);
         }
 
         /**
          * Función que permitirá cerrar un puntero al archivo-
-         * @param FILE $ar puntero al archivo.
+         * @param FILE $file puntero al archivo.
          * @return bool TRUE si se pudo cerrar el archivo. FALSE caso contrario.
+         * @author Varela Vargas Leandro.
          */
-        protected final function cerrarArchivo( $ar ) {
-            return fclose($ar);
+        protected final function closeFile( $file ) {
+            return fclose($file);
+        }
+
+        /**
+         * Función que permitirá agregar una línea sí, solo sí el archivo está en modo Write o Append.
+         * @param String $str_line línea a agregar. NO debe incluir el salto de línea.
+         * @param int $int_length cantidad de caracteres a ser agregados.
+         * @return bool TRUE si se pudo agregar. FALSE caso contrario.
+         * @author Varela Vargas Leandro.
+         */
+        public final function write_appendLine( $file, $str_line, $int_length = null ) {
+            fputs($file, $str_line . "\n", $int_length);
         }
 
     }
