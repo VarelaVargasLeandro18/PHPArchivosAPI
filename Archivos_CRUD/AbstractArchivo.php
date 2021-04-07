d3571198d407adb4770b9f8263222d9bba4117d0
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-07 01:43:49 -0300

    Creación del constructor de la clase.

--- Archivos_CRUD/AbstractArchivo.php
+++ Archivos_CRUD/AbstractArchivo.php
@@ -8,9 +8,18 @@
 
         public function __construct ( $file, $str_pathToFile, $str_fileType ) {
             
+            $isAnyNull = !(isset($file) && isset($str_pathToFile) && isset($str_fileType));
             
+            if ( $isAnyNull )
+                throw new Exception("Ningún atributo puede ser NULL.");
+
+            $this->file = $file;
+            $this->str_pathToFile = $str_pathToFile;
+            $this->str_fileType = $str_fileType;
 
         }
+
+        
 
     }
 
