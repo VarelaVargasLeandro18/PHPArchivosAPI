c992b58069713280d9a5e48629ea18b9c0d4390a
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-08 18:16:18 -0300

    Creada la implementación de la función insertOBJ.

--- Archivos_CRUD/ArchivosJSON.class.php
+++ Archivos_CRUD/ArchivosJSON.class.php
@@ -2,12 +2,24 @@
 
     require_once 'AbstractArchivo.class.php';
 
-    class ArchivoJSON extends AbstractArchivo {
+    class ArchivoJSON extends AbstractArchivo implements iArchivosOBJ {
         
         public function __construct($str_pathToFile) {
             parent::__construct($str_pathToFile, "json");
         }
         
+        public function insertOBJ($obj) {
+            $str_insert = json_encode($obj);
+            $ret = FALSE;
+
+            if ( $str_insert ) {
+                $this->create_openFile("a");
+                $ret = $this->write_appendLine($str_insert);
+            }
+            
+            return $ret;
+        }
+
     }
 
 ?>
