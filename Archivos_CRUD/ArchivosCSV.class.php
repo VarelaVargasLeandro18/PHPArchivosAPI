456df8736499c5e54b3eb67fcb6bb8264339b50d
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-10 10:48:01 -0300

    Creación del constructor de la clase.

--- Archivos_CRUD/ArchivosCSV.class.php
+++ Archivos_CRUD/ArchivosCSV.class.php
@@ -1,5 +1,21 @@
 <?php
 
-    
+    class ArchivosCSV extends AbstractArchivo implements iArchivosOBJ {
+
+
+        private $tableHeader;
+
+        /**
+         * Función constructora de la clase.
+         * @param string $str_pathToFile. Path al archivo.
+         * @param string $tableHeader. Encabezados de columnas.
+         * @author Varela Vargas Leandro
+         */
+        public function __construct($str_pathToFile, $tableHeader= NULL) {
+            parent::__construct($str_pathToFile, "csv");
+            $this->tableHeader = $tableHeader;
+        }
+
+    }
 
 ?>
--- Archivos_CRUD/iArchivosOBJ.php
+++ Archivos_CRUD/iArchivosOBJ.php
@@ -2,6 +2,7 @@
 
     interface iArchivosOBJ {
 
+        public function createFile();
         public function insertOBJ($obj);
         public function readOBJwithValue($value);
         public function deleteOBJwithValue($value);
