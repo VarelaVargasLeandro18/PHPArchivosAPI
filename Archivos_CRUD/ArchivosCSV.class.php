06bcaf9413092c3ec76be1729aa18e0ccf1acf65
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-04-10 13:43:40 -0300

    Implementada funcion readOBJWithValue

--- Archivos_CRUD/ArchivosCSV.class.php
+++ Archivos_CRUD/ArchivosCSV.class.php
@@ -25,7 +25,6 @@
                 
             $this->closeFile();
         }
-        
 
         public function insertOBJ($obj) {
             $ret = FALSE;
@@ -36,6 +35,50 @@
             }
             
             return $ret > 0;
+        }
+
+        public function readOBJwithValue($value) {
+            $ret = NULL;
+            $arr_file = $this->readFile();
+            $lineFileOBJ = $this->findNrLineWithStr($value);
+            $ret = (isset($arr_file) && 
+                    $lineFileOBJ !== FALSE ) ? 
+                    $arr_file[$lineFileOBJ] : NULL;
+
+            if ( $lineFileOBJ !== FALSE && 
+                    isset($this->header) &&
+                    isset($ret) ) { // Si se encuentra una coincidencia...
+
+                $attribs = strtoupper($this->tableHeader);
+                $attribsArr = str_getcsv($attribs); // Separamos los valores del header.
+
+                $valueToUpper = strtoupper($value);
+
+                $posEndAtt = strpos($valueToUpper, ":");
+                $attribToUpper = substr($valueToUpper, 0, $posEndAtt);
+                $attribToUpper = str_replace($attribToUpper, "\"", ''); 
+                // Obtenemos el NOMBRE del atributo pedido.
+
+                $posStartVal = $posEndAtt + 1;
+                $valueToUpper = substr($valueToUpper, $posStartVal);
+                // Obtenemos el VALOR del atributo pedido.
+
+                $keyAttrib = array_search($attribToUpper, $attribsArr);
+                // Buscamos que el NOMBRE del atributo
+                // se encuentre en el array de atributos y obtenemos su posicion.
+                
+                // A continuación comparamos que el valor que tiene el objeto
+                // en la posición del $keyAttrib coincida con $valueToUpper
+                if ( $keyAttrib !== FALSE ) {
+                    $retValue = $ret[$keyAttrib];
+                    $retValueStr = '' . $retValue;
+                    $retValueStr = strtoupper($retValueStr);
+                    $ret = ($retValueStr == $valueToUpper) ? $ret : NULL;
+                }
+
+            } 
+            
+            return $ret;
         }
 
     }
